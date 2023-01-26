3eb9a4430d41aed9465d0ba40f73e68699f4f1fa
OmTaneja <121193126+OmTaneja@users.noreply.github.com>
2023-01-26 16:08:32 +0530

    Add files via upload

---
+++ 4.question1.py
@@ -0,0 +1,14 @@
+marks = int(input('enter your marks :-'))
+
+if marks<25 :
+	print('Grade F')
+elif 25<=marks<=45:
+	print('Grade E')
+elif 45<=marks<=50:
+	print('Grade D')
+elif 50<=marks<=60:
+	print('Grade C')
+elif 60<=marks<=80:
+	print('Grade B')
+else :
+	print('Grade A')
